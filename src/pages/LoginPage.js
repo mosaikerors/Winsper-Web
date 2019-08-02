467b221f138ca79e6b68d4df18872cdd9b000b64
zncai <zinuocai@gmail.com>
2019-08-02 09:22:31 +0800

    fix: modify background color

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,11 @@
 import React from 'react'
 import Login from '../components/login'
+import './LoginPage.css'
 
 export default class LoginPage extends React.Component{
   render(){
     return(
-      <div style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: document.documentElement.offsetHeight,
-        backgroundImage: "url('https://tse3-mm.cn.bing.net/th?id=OIP.xXwDHPUDPPO0MOlasK4xyQAAAA&w=157&h=160&c=7&o=5&dpr=1.5&pid=1.7')",
-      }}>
+      <div className={"background"}>
         <Login/>
       </div>
     )
